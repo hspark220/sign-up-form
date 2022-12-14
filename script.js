3f16c67c08577737419daa3678cc514de04b79bc
Heon Soo Park <hspark220@gmail.com>
2022-12-14 07:46:21 -0500

    Add maxlength and input event for phonenumber
    made '-' appear for the phone number automatically.
    maxlength is used to limit the number of input characters

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // phone number customization
-const areaCode = document.getElementById("areacode");
+const phonenumber = document.getElementById("phonenumber");
+
+phonenumber.addEventListener('input', (event) => {
+    if(phonenumber.value.length == 4 && phonenumber.value[3] != '-') {
+        phonenumber.value = phonenumber.value.slice(0,3) + '-' + phonenumber.value.slice(3);
+    }
+});
