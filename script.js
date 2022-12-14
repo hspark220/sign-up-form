8d74bd6bcc88862a8031e3fbaac6f84ba1332dd4
Heon Soo Park <hspark220@gmail.com>
2022-12-14 08:15:51 -0500

    Add password validation using pattern

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// phone number customization
+// phone number validation / customization
 const phonenumber = document.getElementById("phonenumber");
 
 phonenumber.addEventListener('input', (event) => {
     if(phonenumber.value.length == 4 && phonenumber.value[3] != '-') {
         phonenumber.value = phonenumber.value.slice(0,3) + '-' + phonenumber.value.slice(3);
+    }
+});
+
+// password validation
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+
+
+confirmPassword.addEventListener('change', () => {
+    if(password.value != confirmPassword.value) {
+        
     }
 });
